53caa1fa5548915076dc2545d00e0888c4fd9e99
Maks Nikulnikov <5286433@gmail.com>
2023-05-16 16:27:38 +0300

    feat: add AboutProject component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,7 @@ import Header from "../Header/Header";
 import Navigation from "../Navigation/Navigation";
 import Footer from "../Footer/Footer";
 import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
 
 function Main({ isAuthorized, isMenuActive, onClickBurgerBtn }) {
   return (
@@ -17,6 +18,7 @@ function Main({ isAuthorized, isMenuActive, onClickBurgerBtn }) {
       </Header>
       <main className="main">
         <Promo />
+        <AboutProject />
       </main>
       <Footer />
     </>
